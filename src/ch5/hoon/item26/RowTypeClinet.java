8b59ede8c8e3738d12e533c6f8025180ccedd2b2
byeungoo <dksekfldks65@naver.com>
2021-05-24 21:11:17 +0900

    [구훈] 아이템26 코드 수정

--- src/ch5/hoon/item26/RowTypeClinet.java
+++ src/ch5/hoon/item26/RowTypeClinet.java
@@ -9,11 +9,18 @@ public class RowTypeClinet {
         List list = new ArrayList<>();
 
         list.add(new Coin());
-        Doge doge = (Doge)list.get(0);  //로타입 사용 시 컴파일 레밸에서 에러를 잡을 수 없음. 그래서 제네릭을 사용해야함.
+        //Doge doge = (Doge)list.get(0);  //로타입 사용 시 컴파일 레밸에서 에러를 잡을 수 없음. 그래서 제네릭을 사용해야함.
 
 
         List<?> list2 = new ArrayList<>();
         //list2.add(new Doge());
+
+        Wallet<Coin> wallet = new Wallet<>();
+        //wallet.coin = new Doge();
+
+        List<Doge> animalList= new ArrayList<>();
+        animalList.add(new Doge());
+        wallet.addCoin(animalList);
 
     }
 
@@ -21,8 +28,22 @@ public class RowTypeClinet {
 
     }
 
-    static class Doge{
+    static class Doge extends Coin{
 
     }
+
+    static class Wallet<T extends Coin>{
+
+        public T coin;
+
+        public void addCoin(List<? extends Coin> coins){
+            coins.forEach(coin -> System.out.println(coin));
+
+            List<? extends Coin> x = new ArrayList<Coin>(); //실체화 가능
+
+        }
+
+    }
+
 
 }
